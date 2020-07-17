a4617a0df2ae56bf89c652d61b1b534d1a9e44c3
MarcoFavorito <marco.favorito@gmail.com>
2020-07-17 22:11:15 +0200

    update nn test

--- src/nn.test.ts
+++ src/nn.test.ts
@@ -4,12 +4,12 @@ import { MLP } from './nn';
 test('test MLP', () => {
   const nn = new MLP(2, [2, 2]);
   const output = nn.call([1, 2]);
-  expect(output.length === 2);
+  expect(output.length).toEqual(2);
 });
 
 test('test MLP with zero input', () => {
   const nn = new MLP(2, [32, 32, 32, 32, 2]);
   const output = nn.call([0, 0]);
-  expect(output.length === 2);
-  expect(output === [new Value(0.0), new Value(0.0)]);
+  expect(output[0].data).toEqual(0.0);
+  expect(output[1].data).toEqual(0.0);
 });
