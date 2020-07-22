4707e055d2b0e01585e47d52f9026a352ad219a8
MarcoFavorito <marco.favorito@gmail.com>
2020-07-22 16:59:01 +0200

    fix cspell check

--- demo/app/demo.ts
+++ demo/app/demo.ts
@@ -6,31 +6,7 @@ function sample(population, k) {
   /*
       Chooses k unique random elements from a population sequence or set.
 
-      Returns a new list containing elements from the population while
-      leaving the original population unchanged.  The resulting list is
-      in selection order so that all sub-slices will also be valid random
-      samples.  This allows raffle winners (the sample) to be partitioned
-      into grand prize and second place winners (the subslices).
-
-      Members of the population need not be hashable or unique.  If the
-      population contains repeats, then each occurrence is a possible
-      selection in the sample.
-
-      To choose a sample in a range of integers, use range as an argument.
-      This is especially fast and space efficient for sampling from a
-      large population:   sample(range(10000000), 60)
-
-      Sampling without replacement entails tracking either potential
-      selections (the pool) in a list or previous selections in a set.
-
-      When the number of selections is small compared to the
-      population, then tracking selections is efficient, requiring
-      only a small set and an occasional reselection.  For
-      a larger number of selections, the pool tracking method is
-      preferred since the list takes less space than the
-      set and it doesn't suffer from frequent reselections.
   */
-
   if (!Array.isArray(population))
     throw new TypeError('Population must be an array.');
   var n = population.length;
