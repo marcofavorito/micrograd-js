d221f59005f2bb79138f8bde3211e8efb7ec3364
MarcoFavorito <marco.favorito@gmail.com>
2020-07-22 19:50:24 +0200

    add script to release gh-pages, update ci

---
+++ scripts/update-gh-pages.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+# Run this script from the project root directory.
+
+set -e
+
+# build the library
+yarn run build
+
+# checkout gh-pages branch
+rm -rf demo-build
+mkdir demo-build
+cd demo-build
+git init 
+git remote add origin git@github.com:marcofavorito/micrograd-js.git
+git fetch origin gh-pages
+git checkout gh-pages
+
+# update files
+rm -rf ./*
+cp -r ../demo/* .
+rm ./lib/microgradjs.js
+cp ../dist/microgradjs.js ./lib
+
+
+# commit changes
+git commit -m "Update gh-pages."
+git push origin gh-pages
+
+set +e
+
+# restore working dir
+cd ..
+
